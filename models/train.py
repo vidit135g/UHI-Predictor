f153ae2bbf80c85abc98c197b5bc0ff8f9676f14
Vidit Gupta <vidit135g@gmail.com>
2025-02-26 21:28:24 +0530

    Updated model training and submission scripts for improved accuracy

--- models/train.py
+++ models/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb7eb712ccc40b412787c0b082b7e7d71abc5c5720e323ce8a9ea05bfd5a4376
-size 4251
+oid sha256:d68d8b70a59c892a032f0ac501ef81ababa8c7388c87d3900e76c29dade807fd
+size 3775
